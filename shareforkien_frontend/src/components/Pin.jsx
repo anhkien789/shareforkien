99bcd71d407593a4f38056a9c3c33a7b61912dc0
anhkien789 <33304061+anhkien789@users.noreply.github.com>
2023-06-21 19:14:14 +0700

    add question mark

--- shareforkien_frontend/src/components/Pin.jsx
+++ shareforkien_frontend/src/components/Pin.jsx
@@ -13,8 +13,9 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   const navigate = useNavigate();
   const user = fetchUser();
 
-  const alreadySaved = !!save?.filter((item) => item.postedBy._id === user?.id)
-    ?.length;
+  const alreadySaved = !!save?.filter(
+    (item) => item?.postedBy?._id === user?.id
+  )?.length;
 
   const savePin = (id) => {
     if (!alreadySaved) {
